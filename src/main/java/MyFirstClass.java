227e0514aa5e7b46dfa142948f6d832a5ccf5797
Elena Khokhleva <marley_91@inbox.ru>
2022-02-09 17:35:55 +0400

    Задание 3. Сделайте сценарий логина (updated)

--- src/main/java/MyFirstClass.java
+++ src/main/java/MyFirstClass.java
@@ -1,33 +1,24 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
-import static java.lang.Thread.sleep;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstClass {
-    public static void main(String[] args) throws InterruptedException {
-    System.setProperty("webdriver.gecko.driver", "C:\\project\\testselenium\\drivers\\geckodriver.exe");
+    public static void main(String[] args)  {
+        System.setProperty("webdriver.gecko.driver", "C:\\project\\testselenium\\drivers\\geckodriver.exe");
 
-    WebDriver driver = new FirefoxDriver();
-    WebDriverWait wait = new WebDriverWait(driver, 5);
+            WebDriver driver = new FirefoxDriver();
+            WebDriverWait wait = new WebDriverWait(driver, 10);
 
-    driver.manage().window().maximize();
-    driver.navigate().to ("http://localhost/litecart/admin");
+            driver.manage().window().maximize();
+            driver.navigate().to ("http://localhost/litecart/admin");
 
-    driver.findElement(By.name("username")).sendKeys("admin");
-    driver.findElement(By.name("password")).sendKeys("admin");
-    sleep (2000);
+            driver.findElement(By.name("username")).sendKeys("admin");
+            driver.findElement(By.name("password")).sendKeys("admin");
+            driver.findElement(By.name("login")).click();
+            wait.until(titleIs("My Store"));
 
-    driver.findElement(By.name("login")).click();
-    wait.until(ExpectedConditions.titleIs("My Store"));
-
-    driver.quit();
-}
+            driver.quit();
+    }
 }
