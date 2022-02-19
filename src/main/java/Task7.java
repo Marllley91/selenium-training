41415e4453e1a802206074f8702889e60efa47e5
Elena Khokhleva <marley_91@inbox.ru>
2022-02-19 16:15:17 +0400

    Задание 7. updated_19_02

--- src/main/java/Task7.java
+++ src/main/java/Task7.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -10,38 +11,25 @@ import java.util.List;
 public class Task7 {
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "C:\\project\\testselenium\\drivers\\geckodriver.exe");
-
         WebDriver driver = new FirefoxDriver();
         WebDriverWait wait = new WebDriverWait(driver, 10);
-
         driver.manage().window().maximize();
 
         //***Log in***//
-
         driver.navigate().to("http://localhost/litecart/en/");
-        List<WebElement> items = driver.findElements(By.xpath("//div[@class='image-wrapper']"));
+        List<WebElement> items = driver.findElements(By.xpath("//ul[@class='listing-wrapper products']/li")); //***Найти товары и поместить в items***//
         int size = items.size();
+        System.out.println("Наличие товаров на странице: " + size);
 
-        for (int i = 0; i < size; i++) {
-            boolean hasSticker = isElementPresent(items.get(i), By.xpath(".//div[starts-with(@class,'sticker')]"));
-
-            if (hasSticker) {
-                System.out.println("Товар № " + i + " имеет один стикер");
-            }  else {
-                System.out.println("Элемент не имеет стикера");
-            }
-
+        //***Убедиться, что какждый товар содержит ровно один стикер***//
+        for (WebElement  i : items) {
+            List<WebElement> test = i.findElements(By.xpath(".//div[contains(@class,\"sticker\")]"));
+            System.out.println("Товар содержит " + test.size() + " стикер(ов)");
         }
-            driver.quit();
+
+        driver.quit();
     }
-    static boolean isElementPresent(WebElement element, By locator) {
-        try {
-            element.findElement(locator);
-            return true;
-        } catch (NoSuchElementException ex) {
-            return false;
-        }
-    }
+
 }
 
 
