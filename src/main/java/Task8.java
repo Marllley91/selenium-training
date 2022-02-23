64da7812044959c80c7560c0f34faf30d2850abf
Elena Khokhleva <marley_91@inbox.ru>
2022-02-24 01:18:29 +0400

    Задание 8. Проверить сортировку стран и геозон на странице стран

--- src/main/java/Task8.java
+++ src/main/java/Task8.java
@@ -13,7 +13,6 @@ public class Task8 {
         driver.manage().window().maximize();
 
         //***Log in***//
-
         driver.navigate().to("http://localhost/litecart/admin/?app=countries&doc=countries");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
@@ -27,27 +26,26 @@ public class Task8 {
 
         //*Массив с дефолтным набором стран*//
         List<String> all_countries = new ArrayList<>();
-           for (int a = 0; a < countries.size(); a++) {
-               all_countries.add(countries.get(a).getText());
-           }
+        for (int a = 0; a < countries.size(); a++) {
+            all_countries.add(countries.get(a).getText());
+        }
 
         //* Массив с отсортированным набором стран*//
-         List<String> sort_countries = new ArrayList<>(all_countries);
-         Collections.sort(sort_countries);
+        List<String> sort_countries = new ArrayList<>(all_countries);
+        Collections.sort(sort_countries);
 
 
         //* Сравнение массивов, проверка что страны по алфавиту*//
-             for (int b = 0; b < all_countries.size(); b++) {
-                if (all_countries.get(b).equals(sort_countries.get(b))) {
-                }
-                else {
-                    System.out.println("Список стран НЕ по алфавиту :");
-                }
+        for (int b = 0; b < all_countries.size(); b++) {
+            if (all_countries.get(b).equals(sort_countries.get(b))) {
             }
+            else {
+                System.out.println("Список стран НЕ по алфавиту :");
+            }
+        }
 
         //*Найти страны, у которых зоны > 0*//
-            for (int i=0; i<size1; i++) {
-            zones = driver.findElements(By.xpath("//table[@class='dataTable']//tr[@class='row']//td[6]"));
+        for (int i=0; i<size1; i++) {
             WebElement zone = zones.get(i);
             String test = zone.getAttribute("textContent");
             if (test.equals("0")) {
@@ -58,40 +56,24 @@ public class Task8 {
                 List<WebElement> zone2 = driver.findElements(By.xpath("//table[@id='table-zones']//td[3]"));
                 int size2 = zone2.size() - 1;
                 List<String> all_elements = new ArrayList<>(); //*Массив будет использоваться, как массив со страницы по дефолту*//
-                List<String> all_elements_sort = new ArrayList<>(); //*Массив будет использоваться, как массив с сортировкой*//
                 for (int j=0; j<size2; j++) {
                     zone2 = driver.findElements(By.xpath("//table[@id='table-zones']//td[3]"));
                     all_elements.add(zone2.get(j).getText());
-                    all_elements_sort.add(zone2.get(j).getText());
-                    Collections.sort(all_elements_sort);
                 }
+                List<String> all_elements_sort = new ArrayList<>(all_elements); //*Массив будет использоваться, как массив с сортировкой*//
+                Collections.sort(all_elements_sort);  //*Сортировка массива*//
                 //*Проверка алфавитного порядка стран на странице зоны*//
                 for (int y=0; y<size2; y++) {
                     if (all_elements.get(y).equals(all_elements_sort.get(y))) {
-
                     } else {
                         System.out.println("Страны отображены не по алфавиту");
                         break;
                     }
                 }
                 driver.navigate().back();
+                zones = driver.findElements(By.xpath("//table[@class='dataTable']//tr[@class='row']//td[6]"));
             }
-            }
+        }
         driver.quit();
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
