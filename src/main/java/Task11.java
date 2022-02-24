3cf5436959c6530e9c04c6e9cb28b7a01f80f1fe
Elena Khokhleva <marley_91@inbox.ru>
2022-02-25 01:21:46 +0400

    Задание 11. Сделайте сценарий регистрации пользователя

--- src/main/java/Task11.java
+++ src/main/java/Task11.java
@@ -1,6 +1,5 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
