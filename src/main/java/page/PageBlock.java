da6de78d7dbdd0b21c5f7c0e3fc060047a25fd74
Elena Khokhleva <marley_91@inbox.ru>
2022-03-17 17:13:30 +0400

    Задание 19. Реализовать многослойную архитектуру. Updated. Page_block.java added to repo

---
+++ src/main/java/page/PageBlock.java
@@ -0,0 +1,14 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PageBlock {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    public PageBlock(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+    }
+}
